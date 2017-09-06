14b4e848919a5f6a153eebca05c3aed374183c74
furixturi <alabebop@gmail.com>
2017-09-06 22:17:41 +0900

    allow cors

--- api-server/app.rb
+++ api-server/app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'json'
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
 
 get '/items' do
   data = File.read('./json/all.json')
