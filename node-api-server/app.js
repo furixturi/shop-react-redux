15a563334f0dfb45f4d9c67954720198948923bb
furixturi <alabebop@gmail.com>
2017-09-27 12:26:24 +0900

    add router, read json file and serve jsons

--- node-api-server/app.js
+++ node-api-server/app.js
@@ -1,8 +1,53 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
+})
+
+app.get('/items', function(req, res) {
+    fs.readFile(path.normalize(__dirname + '/json/all.json'),
+        'utf8', 
+        function(err, data) {
+            if(err) {
+                return console.log(err)
+            }
+            res.send(data)
+        }
+    )
+})
+
+app.get('/items/:id', function(req, res) {
+    // const idx = (parseInt(req.params['id']) - 1).toString();
+    const idx = req.params['id'];
+
+    fs.readFile(path.normalize(__dirname + '/json/all.json'), 
+        'utf8', 
+        function(err, data) {
+            if(err) {
+                return console.log(err)
+            }
+            const items = JSON.parse(data)
+            const item = items.data.find((item) => {
+                return item['id'] === idx
+            })
+            res.send(JSON.stringify(item))
+        }
+    )
+})
+
+app.get('/categories', function(req, res) {
+    fs.readFile(path.normalize(__dirname + '/json/categories.json'), 
+        'utf8', 
+        function(err, data) {
+            if(err) {
+                return console.log(err)
+            }
+            res.send(data)
+        }
+    )
 })
 
 app.listen(3000, function() {
